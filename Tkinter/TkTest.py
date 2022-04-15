6dcf60583aaf31e33e7fad3cdfd5b5b022f32577
MilenK <milen_kunchev@abv.bg>
2022-04-15 23:12:40 +0300

    Test commit

--- Tkinter/TkTest.py
+++ Tkinter/TkTest.py
@@ -4,16 +4,19 @@ distance = ' ' * 20
 root = Tk()
 
 # Create a label Widget
-# my_label = Label(root, text='Click the button')
-# my_label.grid(row=0, column=0)
+my_label = Label(root, text='Click the button')
+my_label.grid(row=0, column=0)
+# Add free space
+my_label = Label(root, text=distance)
+my_label.grid(row=0, column=1)
 
 
-def on_click():
-    on_click_label = Label(root, text='Click the button')
-    on_click_label.grid(row=0, column=0)
+# def on_click():
+#     on_click_label = Label(root, text='Click the button')
+#     on_click_label.grid(row=0, column=0)
 
 
-first_button = Button(root, text="BUTTON", command=on_click)
+first_button = Button(root, text="BUTTON")
 first_button.grid(row=0, column=2)
 
 root.mainloop()
