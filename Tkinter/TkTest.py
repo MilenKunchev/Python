fee39c5ba5135702f63a97f33230220b3e2bd5f9
MilenK <milen_kunchev@abv.bg>
2022-04-16 22:40:44 +0300

    Creating input field

--- Tkinter/TkTest.py
+++ Tkinter/TkTest.py
@@ -4,18 +4,19 @@ distance = ' ' * 20
 root = Tk()
 
 # Create a label Widget
-my_label = Label(root, text='Click the button')
+my_label = Label(root, text='Your email')
 my_label.grid(row=0, column=0)
-# Add free space
-my_label = Label(root, text=distance)
-my_label.grid(row=0, column=1)
 
-# def on_click():
-#     on_click_label = Label(root, text='Click the button')
-#     on_click_label.grid(row=0, column=0)
+entry = Entry(root, width=20, bg="white", fg="red", borderwidth=1)
+entry.grid(row=0, column=1)
 
 
-first_button = Button(root, text="BUTTON")
+def on_click_button_one():
+    second_label = Label(root, text=entry.get())
+    second_label.grid(row=1, column=1)
+
+
+first_button = Button(root, text="Submit", command=on_click_button_one)
 first_button.grid(row=0, column=2)
 
 root.mainloop()
