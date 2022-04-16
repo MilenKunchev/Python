b4afc99570d6b736a3d9e3a5c4038c5e839d6922
MilenK <milen_kunchev@abv.bg>
2022-04-16 22:55:36 +0300

    Add default entry text

--- Tkinter/TkTest.py
+++ Tkinter/TkTest.py
@@ -2,13 +2,15 @@ from tkinter import *
 
 distance = ' ' * 20
 root = Tk()
+root.title("Title is here")
 
 # Create a label Widget
 my_label = Label(root, text='Your email')
 my_label.grid(row=0, column=0)
 
-entry = Entry(root, width=20, bg="white", fg="red", borderwidth=1)
+entry = Entry(root, width=20, bg="white", fg="gray", borderwidth=1)
 entry.grid(row=0, column=1)
+entry.insert(0, " Your email")
 
 
 def on_click_button_one():
