979610cefa0475cc4a110760c4d620e6e6c37eaa
MilenK <milen_kunchev@abv.bg>
2022-04-17 13:27:31 +0300

    Put some repeated button parameters in  a dictionary

--- Tkinter/calculator.py
+++ Tkinter/calculator.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.title("Calculator")
 
-entry = Entry(root, width=35, borderwidth=5)
+entry = Entry(root, width=45, bd=4)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 entry.insert(0, "")
 
@@ -12,26 +12,31 @@ def button_click():
     return
 
 
-# define buttons
+number_button = {
+    "padx": "40",
+    "pady": "20",
+    "command": button_click,
 
+}
 
-
-# TODO this is must be other Class in other file
 buttons_pad_x = 40
 buttons_pad_y = 20
-button_1 = Button(root, text="1", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_2 = Button(root, text="2", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_3 = Button(root, text="3", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_4 = Button(root, text="4", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_5 = Button(root, text="5", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_6 = Button(root, text="6", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_7 = Button(root, text="7", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_8 = Button(root, text="8", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_9 = Button(root, text="9", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
-button_0 = Button(root, text="0", padx=buttons_pad_x, pady=buttons_pad_y, command=button_click)
+
+# define buttons
+# TODO this is must be other Class in other file
+button_1 = Button(root, text="1", **number_button)
+button_2 = Button(root, text="2", **number_button)
+button_3 = Button(root, text="3", **number_button)
+button_4 = Button(root, text="4", **number_button)
+button_5 = Button(root, text="5", **number_button)
+button_6 = Button(root, text="6", **number_button)
+button_7 = Button(root, text="7", **number_button)
+button_8 = Button(root, text="8", **number_button)
+button_9 = Button(root, text="9", **number_button)
+button_0 = Button(root, text="0", **number_button)
 button_add = Button(root, text="+", padx=buttons_pad_x - 1, pady=buttons_pad_y, command=button_click)
 button_subtract = Button(root, text="- ", padx=buttons_pad_x - 1, pady=buttons_pad_y, command=button_click)
-button_equal = Button(root, text="= ", padx=buttons_pad_x + 45, pady=buttons_pad_y, command=button_click)
+button_equal = Button(root, text=" = ", padx=buttons_pad_x + 46, pady=buttons_pad_y, command=button_click)
 button_clear = Button(root, text="clr ", padx=buttons_pad_x - 5, pady=buttons_pad_y, command=button_click)
 
 button_1.grid(row=3, column=0)
