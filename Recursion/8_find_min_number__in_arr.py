422a0663515a47b25a04c64ad2cd7cdedd7f35ed
MilenK <milen_kunchev@abv.bg>
2022-05-08 11:19:04 +0300

    some variable names have been changed

--- Recursion/8_find_min_number__in_arr.py
+++ Recursion/8_find_min_number__in_arr.py
@@ -1,19 +1,17 @@
 listA = [9, -2, 6, 1, 80, 9, -2]
 
 
-def findMinimum(l):
+def find_minimum(l):
     if len(l) == 0:
         raise ValueError('Cannot find the minimum of an empty list.')
     elif len(l) == 1:
         return l[0]
     else:
-        minNumber = findMinimum(l[1:])
-        min = l[0]
-        if minNumber < min:
-            min = minNumber
-        return min
+        current_min_number = find_minimum(l[1:])
+        min_num = l[0]
+        if current_min_number < min_num:
+            min_num = current_min_number
+        return min_num
 
 
-findMinimum(listA)
-
-print(findMinimum(listA))
+print(find_minimum(listA))
