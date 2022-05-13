85707d925184403428839c2168d387e87011ded7
MilenK <milen_kunchev@abv.bg>
2022-05-13 20:03:27 +0300

    added 11_1_recursion_limit

---
+++ Recursion/11_1_get_recursion_limit.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def fetch_recursion_limit():
+    def f1(n):
+        try:
+            return f1(n + 1)
+        except RecursionError:
+            return n + 3
+
+    return f1(0)
+
+
+print(fetch_recursion_limit())
+""" Returned code error is for run out of time with 5000 -> new solution is needed """
+
+tests = [200, 899, 1000, 89, 567, 390, 768]
+for test in tests:
+    sys.setrecursionlimit(test)
+    print(fetch_recursion_limit())
